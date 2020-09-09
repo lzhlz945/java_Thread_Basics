25cff7729e1bdd103a6db4787bc87fa25e2bd437
Administrator <1746678613@qq.com>
2020-09-09 10:33:34 +0800

    初始化代码到仓库

---
+++ src/thread/Test02.java
@@ -0,0 +1,33 @@
+package thread;
+
+public class Test02
+{
+    //匿名内部内
+    public static void main(String[] args) {
+        new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    if(i % 2==0){
+                        System.out.println(Thread.currentThread().getName()+":"+i);
+                    }
+                }
+            }
+        }.start();
+        new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    if(i % 2 !=0){
+                        System.out.println(Thread.currentThread().getName()+":"+i);
+                    }
+                    if(i % 20==0){
+                        yield();
+                    }
+                }
+            }
+        }.start();
+
+
+    }
+}
