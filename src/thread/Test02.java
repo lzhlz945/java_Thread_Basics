6e7d8689d518dbf851f34440523f5a72461d87d2
Administrator <1746678613@qq.com>
2020-09-10 15:23:43 +0800

    bran1 上线程交互打印 更新笔记

--- src/thread/Test02.java
+++ src/thread/Test02.java
@@ -14,7 +14,7 @@ public class Test02
                 }
             }
         }.start();
-        new Thread(){
+         new Thread(){
             @Override
             public void run() {
                 for (int i = 0; i < 100; i++) {
